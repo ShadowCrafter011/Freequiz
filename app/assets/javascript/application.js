e8406d19179f39b508a0b66c5e4a3657f336c676
ShadowCrafter011 <lkoe@bluewin.ch>
2022-11-09 14:41:35 +0100

    Added notification toast in layouts/application.html.erb

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,10 @@
+document.addEventListener("turbo:load", () => {
+    const toast = document.getElementById("notification");
+    const notice = document.getElementById("notice");
+    const alert = document.getElementById("alert");
+    const success = document.getElementById("success")
+
+    if (notice.innerHTML || alert.innerHTML || success.innerHTML) {
+        new bootstrap.Toast(toast).show();
+    }
+});
