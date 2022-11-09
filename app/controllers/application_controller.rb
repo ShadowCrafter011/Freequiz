6a886b58d920dc40a0aff6d655341a3a178eca04
ShadowCrafter011 <lkoe@bluewin.ch>
2022-11-09 15:24:10 +0100

    create notifications with gn(**args) in any Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def generate_notification **messages
+        @notifications = messages
+    end
+
+    def gn **messages
+        generate_notification :notice => messages[:n], :alert => messages[:a], :success => messages[:s]
+    end
 end
