106b987a7b6990dc631e908f42270ceca0042e62
ShadowCrafter011 <lkoe@bluewin.ch>
2022-11-16 15:08:16 +0100

    Better user show view. Made scalable

--- app/helpers/user/user_helper.rb
+++ app/helpers/user/user_helper.rb
@@ -1,2 +1,16 @@
 module User::UserHelper
+    def create_items hash
+        data = ""
+        hash.each do |key, value|
+            next unless hash[key].present?
+
+            data += "
+            <div class='row'>
+                <div class='col-6'>#{key}</div>
+                <div class='col-6'>#{hash[key]}</div>
+            </div>
+            "
+        end
+        data.html_safe
+    end
 end
