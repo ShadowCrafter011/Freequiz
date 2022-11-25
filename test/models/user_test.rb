5b950dd72c5e17797fbe2b5443fc8aced4b20ac6
ShadowCrafter011 <lkoe@bluewin.ch>
2022-11-25 19:57:59 +0100

    Wrote User tests and deleted old user fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "do not create user without data" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "user accept agb" do
+    user = User.new user_params_with(agb: "0")
+    assert_not user.save
+  end
+
+  test "user password match" do
+    user = User.new user_params_with(password_confirmation: "HallOO123")
+    assert_not user.save
+  end
+
+  test "user password encrypt" do
+    user = User.create user_params
+    pw_digest = user_params[:password]
+    9.times do
+      pw_digest = Digest::SHA256.hexdigest pw_digest
+    end
+    assert_equal pw_digest, user.password
+  end
+
+  test "create user" do
+    user = User.new user_params
+    assert user.save
+  end
+
+  test "do not create users with the same username" do
+    User.create user_params
+    user = User.new user_params_with(email: "diekillerdrone788@gmail.com")
+
+    assert_not user.save
+  end
+
+  test "do not create users with the same email" do
+    User.create user_params
+    user = User.new user_params_with(username: "hello")
+    assert_not user.save
+  end
+
+  test "email downcase" do
+    user = User.create(user_params_with(email: "lkoE@blUEwin.cH"))
+    assert_equal "lkoe@bluewin.ch", user.email
+  end
+
+  test "confirmation token" do
+    user = User.create(user_params_with(email: "wow@notanemail.wow"))
+    assert user.confirmation_token
+    assert user.confirmation_expire
+    assert_equal user.confirmed, false
+  end
+
+  test "role on creation" do
+    user = User.create(user_params)
+    assert_equal user.role, "user"
+  end
+
+  test "sign in count" do
+    user = User.create(user_params)
+    assert_equal user.sign_in_count, 1
+  end
 end
