574817bc75fb83d01ba2f18470082680428cea23
Lukas Köllner <lkoe@bluewin.ch>
2023-11-17 11:05:57 +0000

    Removed locale_en from before action

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,7 +2,7 @@ class Api::ApiController < ApplicationController
   include ApiUtils
 
   protect_from_forgery with: :null_session
-  before_action :api_require_valid_bearer_token!, :locale_en
+  before_action :api_require_valid_bearer_token!
   around_action :locale_en
   skip_before_action :setup_login
   skip_around_action :switch_locale
