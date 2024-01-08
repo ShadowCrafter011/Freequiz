cce5c3364d04d48380a6b254cdbdb352d04515ae
Lukas Köllner <lkoe@bluewin.ch>
2024-01-08 10:38:34 +0000

    Working on bug reports

---
+++ test/system/bug_reports_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class BugReportsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit bug_reports_url
+  #
+  #   assert_selector "h1", text: "BugReport"
+  # end
+end
