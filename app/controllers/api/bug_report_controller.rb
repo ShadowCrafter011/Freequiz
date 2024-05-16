bc98db6ea29f1067158dc47b9d18db1df3a7b369
Lukas Köllner <lkoe@bluewin.ch>
2024-05-16 08:16:54 +0000

    Require user to be logged in to bug report API

--- app/controllers/api/bug_report_controller.rb
+++ app/controllers/api/bug_report_controller.rb
@@ -2,21 +2,13 @@ class Api::BugReportController < ApplicationController
     include ApiUtils
 
     protect_from_forgery with: :null_session
-    before_action :api_require_valid_bearer_token!
+    before_action :api_require_valid_bearer_token!, :api_require_valid_access_token!
     around_action :locale_en
     skip_before_action :setup_login
     skip_around_action :switch_locale
 
     def create
-        user = api_current_user
-        report =
-            (
-                if user.present?
-                    user.bug_reports.new(bug_report_params)
-                else
-                    BugReport.new(bug_report_params)
-                end
-            )
+        report = api_current_user.bug_reports.new(bug_report_params)
 
         return json({ success: true, message: "Bug report created" }, :created) if report.save
 
