1b7ad4b45aa9c7daaf2e4e345f398d048697d819
Lukas Köllner <lkoe@bluewin.ch>
2024-08-20 07:11:04 +0000

    Removed unused errors

--- app/controllers/errors_controller.rb
+++
@@ -1,5 +0,0 @@
-class ErrorsController < ApplicationController
-    def not_allowed; end
-
-    def beta_wall; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -224,8 +224,4 @@ Rails.application.routes.draw do
             delete "delete/:delete_token", to: "quiz#destroy", as: "quiz_delete"
         end
     end
-
-    # Error routes
-    get "not_allowed", to: "errors#not_allowed", as: "not_allowed"
-    get "beta_wall", to: "errors#beta_wall", as: "beta_wall"
 end
