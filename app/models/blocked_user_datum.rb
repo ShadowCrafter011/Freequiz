ee8fd918382394908173d7b6aded980584fda851
Lukas Köllner <lkoe@bluewin.ch>
2024-08-26 15:16:16 +0000

    Made blocked username lookup way more efficient

--- app/models/blocked_user_datum.rb
+++ app/models/blocked_user_datum.rb
@@ -1,14 +1,5 @@
 class BlockedUserDatum < ApplicationRecord
-    @blocked_usernames = []
-
     def self.username_blocked?(username)
-        if @blocked_usernames.count != BlockedUserDatum.count
-            @blocked_usernames = []
-            BlockedUserDatum.find_each do |blocked|
-                @blocked_usernames.append(blocked.username)
-            end
-        end
-
-        @blocked_usernames.include? username&.downcase
+        BlockedUserDatum.exists? username: username&.downcase
     end
 end
