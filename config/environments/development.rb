7b2f7e2f335637b3512460a030eb84e99f86a51d
Lukas <lkoe@bluewin.ch>
2024-12-17 16:33:12 +0000

    Changed dev port to 3000 and do not force ssl in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,8 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
     # Settings specified here will take precedence over those in config/application.rb.
 
-    config.force_ssl = true
-
     config.hosts << "dev.freequiz.ch"
 
     config.action_mailer.default_url_options = { host: "dev.freequiz.ch" }
