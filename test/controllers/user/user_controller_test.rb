0b199a6d1b60b5d911cfd04a4c57e8ec849b8923
ShadowCrafter011 <lkoe@bluewin.ch>
2024-12-18 08:59:26 +0000

    Added some integration tests to user controller

--- test/controllers/user/user_controller_test.rb
+++ test/controllers/user/user_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class User::UserControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
+    test "can visit sign up page" do
+        get user_create_path
+        assert_response :success
+    end
+
+    test "can visit public page" do
+        get user_public_path(:one)
+        assert_response :success
+    end
+
+    test "can change language without being logged in" do
+        patch change_language_path, params: { locale: :de, return_to: "/" }
+        assert_redirected_to "/"
+    end
+
+    test "do not redirect language change to other domain" do
+        assert_raises(ActionController::Redirecting::UnsafeRedirectError) do
+            patch change_language_path, params: { locale: :de, return_to: "https://google.com" }
+        end
+    end
+
+    test "can create user" do
+        assert_difference "User.count", 1 do
+            post user_create_path, params: { user: {
+                username: "Testing",
+                email: "testing@freequiz.ch",
+                password: "GoodPassword123",
+                password_confirmation: "GoodPassword123",
+                agb: true
+            } }
+            assert_redirected_to user_verification_pending_path
+        end
+    end
+
+    # test "redirect from login page when logged in" do
+    #     sign_in users(:one)
+    #     get user_login_path
+    #     assert_redirected_to user_path
     # end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -9,12 +9,10 @@ class TransactionTest < ActiveSupport::TestCase
 
     test "total value" do
         Transaction.destroy_all
-        total = 0
-        10.times do
-            value = rand(-10..10)
-            total += value
+        values = [10, 200, -20, -15.3, 23.4, -300, 111.1]
+        values.each do |value|
             Transaction.create amount: value, description: "Test one two three"
         end
-        assert_equal total, Transaction.total
+        assert_equal values.sum.round(2), Transaction.total
     end
 end
