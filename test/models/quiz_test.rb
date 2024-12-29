0a138b711de9239a38aa1f546a16fddf451946f6
Lukas <lkoe@bluewin.ch>
2024-12-29 19:54:06 +0000

    Fixed translation amount validation for work for already persisted and new translations

--- test/models/quiz_test.rb
+++ test/models/quiz_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class QuizTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "can create quiz" do
+        assert Quiz.new(
+            title: "Quiz",
+            description: "Description",
+            from: language_id("german"),
+            to: language_id("french"),
+            visibility: "public",
+            translations_attributes: [
+                { word: "Baum", translation: "Arbre" }
+            ]
+        ).save
+    end
+
+    test "cannot create quiz without translations" do
+        assert_not Quiz.new(
+            title: "Quiz",
+            description: "Description",
+            from: language_id("german"),
+            to: language_id("french"),
+            visibility: "public"
+        ).save
+    end
 end
