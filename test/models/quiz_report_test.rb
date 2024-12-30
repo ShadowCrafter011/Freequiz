816f95776599ac1853fb0fed60d7ed8f7bdae59d
Lukas <lkoe@bluewin.ch>
2024-12-30 13:41:48 +0000

    Added quiz report model tests

--- test/models/quiz_report_test.rb
+++ test/models/quiz_report_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class QuizReportTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should not save quiz report without quiz_id" do
+        quiz_report = QuizReport.new
+        assert_not quiz_report.save
+    end
+
+    test "status should be open by default" do
+        quiz_report = QuizReport.new(quiz: quizzes(:one))
+        assert_equal "open", quiz_report.status
+    end
+
+    test "status can be solved" do
+        quiz_report = QuizReport.new(quiz: quizzes(:one), status: "solved")
+        assert quiz_report.valid?
+    end
+
+    test "status can be ignored" do
+        quiz_report = QuizReport.new(quiz: quizzes(:one), status: "ignored")
+        assert quiz_report.valid?
+    end
 end
