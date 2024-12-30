0e2c54f4a7b0f491ce6b087d49168c116f63a7b5
Lukas <lkoe@bluewin.ch>
2024-12-30 13:27:43 +0000

    Languages need name and locale to be valid

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ApplicationRecord
     validates :name, uniqueness: { case_sensitive: false }
     validates :locale, uniqueness: { case_sensitive: false }
+    validates_presence_of :name, :locale
 end
