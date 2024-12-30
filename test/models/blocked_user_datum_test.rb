8396c5e8da82258524489c2eb7d3a15ee4fed61c
Lukas <lkoe@bluewin.ch>
2024-12-30 12:13:18 +0000

    Added tests for blocked usernames

--- test/models/blocked_user_datum_test.rb
+++ test/models/blocked_user_datum_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class BlockedUserDatumTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "freequiz is blocked" do
+        assert BlockedUserDatum.username_blocked?("freequiz")
+    end
+
+    test "freequiz is blocked with extra characters" do
+        assert BlockedUserDatum.username_blocked?("Freequiz123")
+    end
+
+    test "admin is blocked" do
+        assert BlockedUserDatum.username_blocked?("admin")
+    end
+
+    test "other usernames are not blocked" do
+        usernames = %w[
+            Mark
+            John
+            Luke
+            Matthew
+            Shadow123
+            Shadow
+            Shadow_123
+            xXShadowXx
+            ShadowX
+            annyoance
+            abcdefghijklmnopqrstuvwxyz
+            thequickbrownfoxjumpsoverthelazydog123
+            packmyboxwithfivedozenliquorjugs
+            howquicklydaftjumpingzebrasvex
+            sphinxofblackquartzjudgemyvow
+            abcdefghijklmnopqrstuvwxyz98765
+        ]
+
+        usernames.each { |username| assert_not BlockedUserDatum.username_blocked?(username) }
+    end
 end
