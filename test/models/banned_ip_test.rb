669d1acaca8f449cd1dd76f02ca229937156cba2
Lukas <lkoe@bluewin.ch>
2024-12-30 12:08:53 +0000

    Added banned ip model tests

--- test/models/banned_ip_test.rb
+++ test/models/banned_ip_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BannedIpTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "can create a banned ip" do
+        banned_ip = BannedIp.new(ip: "0.0.0.0", reason: "Test")
+        assert banned_ip.save
+    end
+
+    test "can destroy a banned ip" do
+        banned_ip = banned_ips(:one)
+        assert banned_ip.destroy
+    end
 end
