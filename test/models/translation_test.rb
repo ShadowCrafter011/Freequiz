4a42677fb76bf390af6aba33def7e8758c724964
Lukas <lkoe@bluewin.ch>
2024-12-30 13:59:43 +0000

    Added translation model tests

--- test/models/translation_test.rb
+++ test/models/translation_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class TranslationTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should not save translation without quiz" do
+        translation = Translation.new(word: "Hello", translation: "Hallo")
+        assert_not translation.save
+    end
+
+    test "should save translation with quiz" do
+        quiz = quizzes(:one)
+        translation = Translation.new(word: "Hello", translation: "Hallo", quiz: quiz)
+        assert translation.save
+    end
+
+    test "can destroy translation" do
+        translation = translations(:one)
+        assert translation.destroy
+        assert_not Score.find_by(translation_id: translation.id)
+    end
 end
