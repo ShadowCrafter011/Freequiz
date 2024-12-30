3cf93f2f049838979aa1440974af097ac3e59837
Lukas <lkoe@bluewin.ch>
2024-12-30 13:28:01 +0000

    Added language model tests

--- test/models/language_test.rb
+++ test/models/language_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class LanguageTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should not save language without attributes" do
+        language = Language.new
+        assert_not language.save
+    end
+
+    test "should not save language without name" do
+        language = Language.new(locale: "en")
+        assert_not language.save
+    end
+
+    test "should not save language wihout locale" do
+        language = Language.new(name: "English")
+        assert_not language.save
+    end
+
+    test "should not save duplicate language" do
+        language = Language.new(name: "English", locale: "en")
+        assert_not language.save
+    end
 end
