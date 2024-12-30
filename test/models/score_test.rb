891dc595277c830ba26df95d1b5b9213acce727f
Lukas <lkoe@bluewin.ch>
2024-12-30 13:44:14 +0000

    Added score model tests

--- test/models/score_test.rb
+++ test/models/score_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ScoreTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should not save score without user_id" do
+        score = Score.new(translation: translations(:one))
+        assert_not score.save
+    end
+
+    test "should not save score without translation_id" do
+        score = Score.new(user: users(:one))
+        assert_not score.save
+    end
+
+    test "should save score with user_id and translation_id" do
+        score = Score.new(user: users(:one), translation: translations(:two))
+        assert score.save
+    end
+
+    test "should not save score with duplicate user_id and translation_id" do
+        score = Score.new(user: users(:one), translation: translations(:one))
+        assert_not score.save
+    end
 end
