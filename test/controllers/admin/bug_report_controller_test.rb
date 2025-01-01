2a4f7f98a62496e54abbe1d19c669736078340e6
Lukas <lkoe@bluewin.ch>
2025-01-01 16:49:49 +0000

    Moved bug report controller test to admin namespace

--- test/controllers/admin/bug_report_controller_test.rb
+++ test/controllers/admin/bug_report_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class BugReportControllerTest < ActionDispatch::IntegrationTest
+class Admin::BugReportControllerTest < ActionDispatch::IntegrationTest
     # test "the truth" do
     #   assert true
     # end
