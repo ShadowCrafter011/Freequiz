76cb4bbdf32a8a6c9007f170ee7ea75b4441f0a9
Lukas <lkoe@bluewin.ch>
2025-01-01 16:37:31 +0000

    Added home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    test "can get root" do
+        get root_url
+        assert_response :success
+    end
+
+    test "can get root when signed in" do
+        sign_in :one
+        get root_url
+        assert_response :success
+    end
+
+    test "can get sponsors" do
+        get sponsors_url
+        assert_response :success
+    end
+
+    test "can get terms of service" do
+        get terms_of_service_url
+        assert_response :success
+    end
+
+    test "can get privacy policy" do
+        get privacy_policy_url
+        assert_response :success
+    end
+
+    test "can get security policy" do
+        get security_policy_url
+        assert_response :success
+    end
+
+    test "can search quizzes" do
+        get search_url
+        assert_response :success
+    end
 end
