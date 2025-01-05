aaa3563b2d3758208aef94857174b7e0a30ebdbf
Lukas <lkoe@bluewin.ch>
2025-01-05 14:52:03 +0000

    Added API controller test

--- test/controllers/api/api_controller_test.rb
+++ test/controllers/api/api_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class Api::ApiControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should get languages" do
+        get api_languages_url
+
+        json = @response.parsed_body
+
+        assert_response :success
+        assert_equal true, json["success"]
+        assert_not_nil json["data"]
+        assert_equal Language.count, json["data"].length
+    end
 end
