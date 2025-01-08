44ec9e2b4ba74f4f131ff086cb129d480e81abd5
Lukas <lkoe@bluewin.ch>
2025-01-08 14:34:34 +0000

    Switched away from headless chrome to be able to see tests in noVNC

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -13,7 +13,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
               else
                   { browser: :chrome }
               end
-    driven_by :selenium, using: :headless_chrome, options: options
+    driven_by :selenium, using: :chrome, options: options
 
     def find_test_id(test_id)
         find(:test_id, test_id)
