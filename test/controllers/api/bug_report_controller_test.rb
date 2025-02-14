d472d313b80d68508037e8c264b7e9635419d3a5
Lukas <lkoe@bluewin.ch>
2025-02-14 17:01:04 +0000

    Added docs and bug report API tests

--- test/controllers/api/bug_report_controller_test.rb
+++ test/controllers/api/bug_report_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class Api::BugReportControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    test "create bug report" do
+        assert_changes "BugReport.count" do
+            put api_bug_create_path, headers: api_sign_in(:one), params: {
+                bug_report: {
+                    title: "Test",
+                    body: "Test",
+                    url: "http://example.com",
+                    platform: "Test",
+                    user_agent: "Test"
+                }
+            }
+            assert_response :success
+        end
+    end
+
+    test "cannot create bug report when not logged in" do
+        assert_no_changes "BugReport.count" do
+            put api_bug_create_path, params: {
+                bug_report: {
+                    title: "Test",
+                    body: "Test",
+                    url: "http://example.com",
+                    platform: "Test",
+                    user_agent: "Test"
+                }
+            }
+            assert_response :unauthorized
+        end
+    end
 end
--- test/controllers/api/docs_controller_test.rb
+++ test/controllers/api/docs_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class Api::DocsControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    test "docs viewing permission" do
+        pathes = [
+            api_docs_path,
+            api_docs_users_path,
+            api_docs_quizzes_path,
+            api_docs_authentication_path,
+            api_docs_general_errors_path,
+            api_docs_bugs_path,
+            api_docs_languages_path
+        ]
+
+        pathes.each do |path|
+            get path
+            assert_response :not_found
+        end
+
+        sign_in :admin
+
+        pathes.each do |path|
+            get path
+            assert_response :success
+        end
+    end
 end
